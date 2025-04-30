c4d7f71c713f3d656e5edd03789d4f2e9dd65915
sarita-ctrl <saritaarte@gmail.com>
2025-04-30 11:50:26 +0100

    Starting project

---
+++ src/ca_2_2024216/hospital/CA_2_2024216Hospital.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca_2_2024216.hospital;
+
+/**
+ *
+ * @author sary_
+ */
+public class CA_2_2024216Hospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
